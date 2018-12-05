cc0365cc69ddb949262586c231dd4edb3c464540
lia2353 <yana97@abv.bg>
2018-12-05 21:32:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main()
 {
     OperationParser parser;
 
-    //asks for operation (create , sequentialSearch , exit)
+    //asks for operation (create, sequentialSearch, update, delete, exit)
     cin >> parser;
 
     //asks the user to enter an operation till entering exit
     while (parser.getOperation() != "exit")
 	{
 		parser.doOperation();
-		//asks for operation (create , sequentialSearch , exit)
+		//asks for operation (create, sequentialSearch, update, delete, exit)
 		cin >> parser;
 	}
     return 0;
